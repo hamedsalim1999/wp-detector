5152cb335334a052bfbe3184de6e23685066fb56
hamed <hamediran1999@gmail.com>
2021-08-20 14:31:16 +0430

    add some commanest and change varible name

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import requests
 import re
+#  this function was detected wp web site
 def wp_find(url):
     try:
-        respinse = requests.get(url)
-        if respinse.status_code == 200:
-            data = str(respinse.content)
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = str(response.content)
             re_path = re.compile(r'wp-[/\w+/g]')
-            resualt = re.search(re_path,data)
-            if resualt:
+            result = re.search(re_path,data)
+            if result:
                 print("this is wp ")
             else:
                 print("it's not wp")
         else:
-            print(f"your response code was {respinse.status_code}")
+            print(f"your response code was {response.status_code}")
     except:
         print("we cant find your web site")
+        
+# this function was check url pattern 
 def url_check(url):
     re_path=r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
     if bool(re.match(re_path,url)):
@@ -22,11 +25,10 @@ def url_check(url):
     # if bool(re.match(re_path,url)):
         wp_find(url)
     else:
-        print("your url was not coreect plese sure you have been use http or https on first of your url")
+        print("your url was not correct please sure you have been use http or https on first of your url")
 def help():
     print("""
           this program will help you to find wordpress web site 
-          
           
           your url is : <your target website url>
           
