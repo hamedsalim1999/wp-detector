bee6d26ab829b151090eadbdaafaec762eaf5b5a
hamed <hamediran1999@gmail.com>
2021-08-21 10:47:11 +0430

    return result on url check method

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask , jsonify , render_template
+from flask import Flask , jsonify , render_template,request
 from wp_detector import url_check
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello(name=None):
     return render_template('index.html')
+
+@app.route('/url',methods=["POST"])
+def api_api_single_check():
+    data = request.get_json()
+    return url_check(data['url'])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- wp_detector.py
+++ wp_detector.py
@@ -1,7 +1,6 @@
 from sys import version
 import requests
 import re
-from flask import jsonify
 #  this function was detected wp web site
 def wp_find(url):
     try:
@@ -12,23 +11,21 @@ def wp_find(url):
             re_path_version = re.compile(r'WordPress [0-9]?[0-9].[0-9]?[0-9]')
             result = re.search(re_path_version,data)
             if result:
-                return jsonify(wp=True,version=result.group())
+                return {"wp":True,"version":result.group()}
             elif re.search(re_path_wp,data):
-                return jsonify(wp=True,version="cant detect")
+                return {"wp":True,"version":"cant detect"}
             else:
-                return jsonify(wp=False,version=None)
+                return {"wp":False,"version":None}
                 
         else:
-            jsonify(err=f"your response code was {response.status_code}")
+            return{"err":f"your response code was {response.status_code}"}
     except:
-        jsonify(err="we cant find your web site")
+        return{"err":"we cant find your web site"}
         
 # this function was check url pattern 
 def url_check(url):
     re_path=r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
     if bool(re.match(re_path,url)):
-
-    # if bool(re.match(re_path,url)):
-        wp_find(url)
+        return wp_find(url)
     else:
-        jsonify(err="your url was not correct please sure you have been use http or https on first of your url")
+        return{"err":"your url was not correct please sure you have been use http or https on first of your url"}
