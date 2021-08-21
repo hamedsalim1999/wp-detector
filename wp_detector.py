47281943f84a9615f8bee18a2f283e369e12a04f
hamed <hamediran1999@gmail.com>
2021-08-21 20:22:06 +0430

    fix structure return

--- wp_detector.py
+++ wp_detector.py
@@ -11,16 +11,16 @@ def wp_find(url):
             re_path_version = re.compile(r'WordPress [0-9]?[0-9].[0-9]?[0-9]')
             result = re.search(re_path_version,data)
             if result:
-                return {"wp":True,"version":result.group()}
+                return {"url":url,"resualt":"it's wordprees","version":result.group(),"msg":None}
             elif re.search(re_path_wp,data):
-                return {"wp":True,"version":"cant detect"}
+                return {"url":url,"resualt":"it's wordprees","version":"cant detect","msg":None}
             else:
-                return {"wp":False,"version":None}
+                return {"url":url,"resualt":"it's not wordprees","version":None,"msg":None}
                 
         else:
-            return{"err":f"your response code was {response.status_code}"}
+            return{"url":url,"resualt":"err","version":None,"msg":f"your response code was {response.status_code}"}
     except:
-        return{"err":"we cant find your web site"}
+        return{"url":url,"resualt":"err","version":None,"msg":"we cant find your web site"}
         
 # this function was check url pattern 
 def url_check(url):
@@ -28,4 +28,4 @@ def url_check(url):
     if bool(re.match(re_path,url)):
         return wp_find(url)
     else:
-        return{"err":"your url was not correct please sure you have been use http or https on first of your url"}
+        return{"url":url,"resualt":"err","version":None,"msg":"your url was not correct please sure you have been use http or https on first of your url"}
